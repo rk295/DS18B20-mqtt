e31a1eea2cd6311ca0e0108cd9cbdfd8776a8a17
Robin Kearney <robin@kearney.co.uk>
2016-08-03 07:31:08 +0100

    I don't think its the job of this script to run modprobe

--- temp.py
+++ temp.py
@@ -22,9 +22,6 @@ password = os.getenv('MQTT_PASSWORD', None)
 
 auth = {'username':username, 'password':password }
 
-os.system('modprobe w1-gpio')
-os.system('modprobe w1-therm')
-
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
